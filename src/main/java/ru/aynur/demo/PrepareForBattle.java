1a3d7e98f3b3e8e28bca32ad2c364e0aec1e4b57
Aynur Aglyamov <Aynur.Aglyamov@x5.ru>
2021-05-03 01:14:22 +0500

    Simulation a battle Rome and Carthego.

--- src/main/java/ru/aynur/demo/PrepareForBattle.java
+++ src/main/java/ru/aynur/demo/PrepareForBattle.java
@@ -8,10 +8,14 @@ import org.springframework.stereotype.Component;
 public class PrepareForBattle implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        int enemyWarriors = (int) (Math.random() * 100) + 100;
-        String battleStatus = "undefined";
 
-        delegateExecution.setVariable("enemyWarriors", enemyWarriors);
-        delegateExecution.setVariable("battleStatus", battleStatus);
+        int warriorsRome = (int) delegateExecution.getVariable("warriorsRome");
+        int warriorsCarthage = (int) (Math.random() * 100) + 100;
+
+        delegateExecution.setVariable("warriorsRome", warriorsRome);
+        delegateExecution.setVariable("warriorsCarthage", warriorsCarthage);
+
+        delegateExecution.setVariable("romeWon", warriorsRome > warriorsCarthage);
+
     }
 }
